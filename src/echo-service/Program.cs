ca3a476192d9378179fe8a467beb295ddf84a379
zabronax <zabronax@gmail.com>
2025-08-21 16:50:24 +0200

    update echo-service

--- src/echo-service/Program.cs
+++ src/echo-service/Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Welcome EchoService");
+app.MapGet("/", () => "Welcome to EchoService");
 app.MapGet("/healthz", () => "OK");
-app.MapGet("/version", () => "v0.1");
+app.MapGet("/version", () => "v0.2");
 
 app.Run();
